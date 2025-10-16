57c6fbb28968e9d22237d698688604dd19c30060
AllogeneSSRD <1252231934@qq.com>
2025-10-17 00:50:08 +0100

    修复config加载脚本修改后文件的错误

--- Wplace/config.py
+++ Wplace/config.py
@@ -20,20 +20,23 @@ def calculate_crop_coordinates(base_coordinates, offset):
 
 def update_config(config):
     # 更新配置
-    config['block_cords'] = calculate_coordinates(
-        config['top_left_block'],
-        config['block_range']
-    )
+    try:
+        config['block_cords'] = calculate_coordinates(
+            config['top_left_block'],
+            config['block_range']
+        )
 
-    config['merge']['crop_coordinates_png'] = calculate_crop_coordinates(
-        config['upper_left_pixel'] + config['lower_right_pixel'],
-        [0, 0, 1, 1]
-    )
+        config['merge']['crop_coordinates_png'] = calculate_crop_coordinates(
+            config['upper_left_pixel'] + config['lower_right_pixel'],
+            [0, 0, 1, 1]
+        )
 
-    config['merge']['crop_coordinates_jpg'] = calculate_crop_coordinates(
-        config['upper_left_pixel'] + config['lower_right_pixel'],
-        [-1, -1, 2, 2]
-    )
+        config['merge']['crop_coordinates_jpg'] = calculate_crop_coordinates(
+            config['upper_left_pixel'] + config['lower_right_pixel'],
+            [-1, -1, 2, 2]
+        )
+    except Exception as e:
+        print(f"更新配置: {e}")
     return config
 
 
@@ -50,40 +53,48 @@ class ConfigHandler(FileSystemEventHandler):
             # pprint(config)
             return config
 
+    def reload_config(self):
+        with open(self.config_path, 'r', encoding='utf-8') as f:
+            config = yaml.safe_load(f)
+            return config
+
     def on_modified(self, event):
         if event.src_path.endswith(self.config_path):
             print("配置文件已更新，重新加载...")
-            self.config = self.load_config()
+            self.config = update_config(self.reload_config())
             self.reload_event.set()  # 通知主循环重新加载
+            # pprint(self.config)
+            # 在这里可以添加更新后的回调操作
 
+    # --- 新增的属性访问方法 ---
     @property
     def block_cords(self):
-        """监控区块坐标列表"""
+        """返回监控区块坐标列表"""
         return self.config.get('block_cords', [])
     @property
     def monitor(self):
-        """监控区块配置"""
+        """返回监控区块坐标列表"""
         return self.config.get('monitor', {})
     @property
     def merge(self):
-        """图像处理配置"""
+        """返回图像处理配置"""
         return self.config.get('merge', {})
     @property
     def table(self):
-        """表格处理配置"""
+        """返回图像处理配置"""
         return self.config.get('table', {})
 
 
     @property
     def merge_interval(self):
-        """区块合并间隔"""
+        """返回合并处理间隔"""
         return self.config.get('merge', {}).get('interval', 3600)
     @property
     def timeline_file(self):
-        """时间线文件路径"""
+        """返回时间线文件路径"""
         return self.config.get('merge', {}).get('timeline_file', '')
 
 
 if __name__ == "__main__":
-    cfg = ConfigHandler(config_path, None)
-    pprint(cfg.config)
+    cfg_handler = ConfigHandler('config.yaml', None)
+    pprint(cfg_handler.config)
