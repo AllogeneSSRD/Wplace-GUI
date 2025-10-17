b5d6d2b6c55e4d7599629dcff6af46615704af44
AllogeneSSRD <1252231934@qq.com>
2025-10-17 19:19:03 +0100

    更新pathlib

--- main_GUI.py
+++ main_GUI.py
@@ -1,25 +1,27 @@
 
 from pathlib import Path
-from Wplace.find_last_png import find_last_one
+from Wplace.find_last_png import find_last_one, find_color
 
-base_folder = '模板'
+base_folder = Path('模板')
 
-main_exe = (Path(base_folder) / 'Wplace-GUI.exe').resolve()
-image_process_exe = (Path(base_folder) / 'image_process.exe').resolve()
-config_GUI_exe = (Path(base_folder) / 'config_GUI.exe').resolve()
+main_exe = (base_folder / 'Wplace-GUI.exe').resolve()
+image_process_exe = (base_folder / 'image_process.exe').resolve()
+config_GUI_exe = (base_folder / 'config_GUI.exe').resolve()
 
-template = (Path(base_folder) / 'template.png').resolve()
+template = (base_folder / 'template.png').resolve()
 
-timeline_folder = (Path(base_folder) / 'timeline').resolve()
-timeline = find_last_one(timeline_folder, r'^(\d{8})_(\d{6})\.png$')
+timeline_folder = (base_folder / 'timeline').resolve()
+timeline = find_last_one(timeline_folder, r'^(\d{8})_(\d{6}).png$')
 
-timeline_cropped_folder = (Path(base_folder) / 'timeline_cropped_png').resolve()
-timeline_cropped = find_last_one(timeline_cropped_folder, r'^(\d{8})_(\d{6})\.png$')
+timeline_cropped_folder = (base_folder / 'timeline_cropped_png').resolve()
+timeline_cropped = find_last_one(timeline_cropped_folder, r'^(\d{8})_(\d{6}).png$')
 
-timeline_color_folder = (Path(base_folder) / 'timeline_color').resolve()
-timeline_color_finish = find_last_one(timeline_color_folder, r'^finish_all_(\d{8})_(\d{6})\.png$')
-timeline_color_mask = find_last_one(timeline_color_folder, r'^mask_all_(\d{8})_(\d{6})\.png$')
-timeline_color_todo = find_last_one(timeline_color_folder, r'^todo_all_(\d{8})_(\d{6})\.png$')
+timeline_color_folder = (base_folder / 'timeline_color').resolve()
+timeline_color_finish = find_last_one(timeline_color_folder, r'^finish_all_(\d{8})_(\d{6}).png$')
+timeline_color_mask = find_last_one(timeline_color_folder, r'^mask_all_(\d{8})_(\d{6}).png$')
+timeline_color_todo = find_last_one(timeline_color_folder, r'^todo_all_(\d{8})_(\d{6}).png$')
+
+timeline_color_list = find_color(timeline_color_folder, r'^_mask_#(\d{2}).*.png$')
 
 
 
@@ -34,3 +36,4 @@ if __name__ == "__main__":
     print(f"Latest finish_all file in color folder: {timeline_color_finish}")
     print(f"Latest mask_all file in color folder: {timeline_color_mask}")
     print(f"Latest todo_all file in color folder: {timeline_color_todo}")
+    print(f"Latest color file in color folder: {timeline_color_list}")
